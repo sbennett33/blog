edc5e05edcc97c2cfd738af4f8da85d810647984
Scott Bennett <sbennett@change.org>
2022-01-14 14:37:52 -0800

    New name and color scheme

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :blog, BlogWeb.Endpoint,
-  url: [scheme: "https", host: "boredwax.com", port: 443],
+  url: [scheme: "https", host: "patternmatching.io", port: 443],
   force_ssl: [hsts: true, rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
